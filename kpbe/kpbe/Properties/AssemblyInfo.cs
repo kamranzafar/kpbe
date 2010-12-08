9079d5be02472fdb1cda9834aa7c210eaee79110
Kamran <xeus.man@gmail.com>
2010-12-08 11:01:13 +0000

    updates

--- kpbe/kpbe/Properties/AssemblyInfo.cs
+++ kpbe/kpbe/Properties/AssemblyInfo.cs
@@ -25,7 +25,4 @@ using System.Runtime.InteropServices;
 // The assembly version has following format :
 //
 // Major.Minor.Build.Revision
-//
-// You can specify all the values or you can use the default the Revision and 
-// Build Numbers by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0")]
