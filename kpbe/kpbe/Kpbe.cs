634be2610e0179ac8bc32b64454aa7a4831a8901
Kamran <xeus.man@gmail.com>
2010-12-08 22:11:48 +0000

    updates

--- kpbe/kpbe/Kpbe.cs
+++ kpbe/kpbe/Kpbe.cs
@@ -1,10 +1,20 @@
 ﻿/*
- * Created by SharpDevelop.
- * User: Kamran
- * Date: 08/12/2010
- * Time: 20:19
+ * kpbe is a password based file encryption utility
+ * Copyright (C) 2010 Kamran
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
  */
 using System;
 
